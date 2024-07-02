24ce195be09a12f6780a5863500960fc3ba0feb1
deathstroke-Assassin <moh2004amed@gmail.com>
2024-07-02 09:55:00 +0000

    juj

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,17 @@
-export default function getListStudents() {
-  return [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' },
-  ];
+// studentList.js
+
+/**
+ * Returns an array of student objects.
+ * Each object has three attributes: id (Number), firstName (String), and location (String).
+ * @returns {Array} Array of student objects
+ */
+function getListStudents() {
+    const students = [
+        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+        { id: 2, firstName: 'James', location: 'Columbia' },
+        { id: 5, firstName: 'Serena', location: 'San Francisco' }
+    ];
+    return students;
 }
+
+module.exports = getListStudents;
