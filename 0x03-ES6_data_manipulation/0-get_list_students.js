078e5e75cb66dd3a0d0d3721422856f1fea4e1b0
deathstroke-Assassin <moh2004amed@gmail.com>
2024-07-02 08:27:13 +0000

    g

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,8 @@
-function getListStudents() {
-  // code to be executed
-  const list = [
-	  {id: 1, firstName: "Guillaume", location: "San Francisco"},
-	  {id: 2, firstName: "James", location: "Columbia"},
-	  {id: 3, firstName: "Serena", location: "San Francisco"}
-  ];
-  return list;
+export default function getListStudents() {
+	const list = [
+		{id: 1, firstName: "Guillaume", location: "San Francisco"},
+		{id: 2, firstName: "James", location: "Columbia"},
+		{id: 3, firstName: "Serena", location: "San Francisco"}
+	];
+	 return list;
 }
